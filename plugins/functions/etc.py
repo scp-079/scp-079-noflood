5b5ea9a1de9243a05538250ed7f130af8caa32f7
Xiao <xiao@scp-079.org>
2019-10-07 13:41:14 +0000

    - 0.0.6

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -223,7 +223,7 @@ def get_forward_name(message: Message) -> str:
             text = chat.title
 
         if text:
-            text = t2s(text)
+            text = normal(text)
     except Exception as e:
         logger.warning(f"Get forward name error: {e}", exc_info=True)
 
@@ -240,7 +240,7 @@ def get_full_name(user: User) -> str:
                 text += f" {user.last_name}"
 
         if text:
-            text = t2s(text)
+            text = normal(text)
     except Exception as e:
         logger.warning(f"Get full name error: {e}", exc_info=True)
 
@@ -377,6 +377,17 @@ def message_link(message: Message) -> str:
     return text
 
 
+def normal(text: str) -> str:
+    # Convert the string
+    try:
+        if glovar.zh_cn:
+            text = convert(text, config="t2s.json")
+    except Exception as e:
+        logger.warning(f"T2S error: {e}", exc_info=True)
+
+    return text
+
+
 def random_str(i: int) -> str:
     # Get a random string
     text = ""
@@ -384,17 +395,6 @@ def random_str(i: int) -> str:
         text = "".join(choice(ascii_letters + digits) for _ in range(i))
     except Exception as e:
         logger.warning(f"Random str error: {e}", exc_info=True)
-
-    return text
-
-
-def t2s(text: str) -> str:
-    # Convert the string
-    try:
-        if glovar.zh_cn:
-            text = convert(text, config="t2s.json")
-    except Exception as e:
-        logger.warning(f"T2S error: {e}", exc_info=True)
 
     return text
 
