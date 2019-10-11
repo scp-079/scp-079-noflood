8d601470419f6bcef53870caeff532aa1155b354
Xiao <xiao@scp-079.org>
2019-10-11 04:20:02 +0000

    - adjust code style

--- plugins/handlers/command.py
+++ plugins/handlers/command.py
@@ -105,7 +105,7 @@ def config(client: Client, message: Message) -> bool:
 
 
 @Client.on_message(Filters.incoming & Filters.group & ~test_group & from_user
-                   & Filters.command(["config_noflood"], glovar.prefix))
+                   & Filters.command([f"config_{glovar.sender.lower()}"], glovar.prefix))
 def config_directly(client: Client, message: Message) -> bool:
     # Config the bot directly
 
